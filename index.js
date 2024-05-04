4fe2311322c2754a2eb039d807491cf9d8807b23
Shahriar Fardows <shahriarfardows@gmail.com>
2024-05-04 13:48:39 +0600

    full server are ready to deploy

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors(
-  {
-    origin: ['http://localhost:5173'],
-    credentials: true
-  }
-));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
+
+// token verify
+console.log();
+const verify = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.TOKEN, (err, decoded) => {
+    // if (err) {
+    //   return res.status(401).send({ message: "unauthorized access" });
+    // }
+    
+    req.user = decoded;
+    next();
+  });
+};
+
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@phero.sfyzbqi.mongodb.net/?retryWrites=true&w=majority&appName=phero`;
 
@@ -38,28 +57,26 @@ async function run() {
     const database = client.db("phero").collection("project-1");
     const booking = client.db("phero").collection("booking");
 
-    // JWT Token api 
+    // JWT Token api
 
-    app.post('/jwt' , ( req , res) => {
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      console.log(user);
-      const token = jwt.sign( user, 'secret', { expiresIn: '1h' });
-      console.log(token);
+      const token = jwt.sign(user, "secret", { expiresIn: "1h" });
       res
-      .cookie('token' , token , {
+      .cookie('token', token, {
         httpOnly: true,
-        secure: true,
-        sameSite: "none"
-      })
-      .send({ success : true })
+        secure: process.env.NODE_ENV === 'production', 
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+
     })
+        .send({ success: true });
+    });
 
-    app.post('/logOut' , (req , res) => {
+    app.post("/logOut", (req, res) => {
       const user = req.body;
-      res.clearCookie('token',{maxAge : 0}).send({success : true})
-    } )
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
 
- 
     // All data api
     app.get("/allData", async (req, res) => {
       const cursor = database.find();
@@ -79,43 +96,49 @@ async function run() {
 
     // Bookings
 
-        // Bookings
+    app.get("/bookings", verify, async (req, res) => {
+      // console.log("email", req.query.email);
+      // console.log("owner", req.user.email);
+      // if (req.user.email !== req.query.email) {
+      //   return res.status(403).send({ message:  " custom  forbidden access" });
+      // }
 
-        app.get("/bookings" , async (req, res) => {
-          let query = {};
-          if (req.query?.email) {
-            query = { email: req.query.email };
-          }
-          const result = await booking.find(query).toArray();
-          res.send(result);
-        });
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await booking.find(query).toArray();
+      res.send(result);
+    });
+
     
-        app.post("/bookings", async (req, res) => {
-          const info = req.body;
-          const result = await booking.insertOne(info);
-          res.send(result);
-        });
-    
-        app.patch("/bookings/:id", async (req, res) => {
-          const data = req.body;
-          const id = req.params.id;
-          const filter = { _id: new ObjectId(id) };
-          const updateDoc = {
-            $set: {
-              status: data.status,
-            },
-          };
-          const result = await booking.updateOne(filter, updateDoc);
-          res.send(result);
-        });
-    
-        app.delete("/bookings/:id", async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: new ObjectId(id) };
-          const result = await booking.deleteOne(query);
-          res.send(result);
-        });
-        
+
+    app.post("/bookings", async (req, res) => {
+      const info = req.body;
+      const result = await booking.insertOne(info);
+      res.send(result);
+    });
+
+    app.patch("/bookings/:id", async (req, res) => {
+      const data = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await booking.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await booking.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
